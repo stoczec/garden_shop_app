cfca662d24ada4fe54b2dc81a3372ecc2feae58b
Dmytro Geraschenko <stoczec@gmail.com>
2023-07-18 21:44:06 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './footer.module.scss';
 import { Map } from '../Map/Map';
+import { social_networks } from '../../data/socialNetworks';
 
 export const Footer = () => {
   return (
@@ -9,13 +10,24 @@ export const Footer = () => {
         <article className={style.contact}>
           <h3>Contact</h3>
           <p>+49 999 999 99 99</p>
-          <div className={style.social_links}></div>
+          <div className={style.social_links}>
+            {social_networks.map(({ id, name, logo, src }) => {
+              return (
+                <a key={id} href={src} target="_blank">
+                  <img src={logo} alt={name} />
+                  <span>{name}</span>
+                </a>
+              );
+            })}
+          </div>
         </article>
         <article className={style.adress}>
           <h3>Address</h3>
-          <p>Linkstraße 2, 8 OG, 10785, Berlin, Deutschland</p>
-          <p>Working Hours:</p>
-          <p>24 hours a day</p>
+          <a href="https://goo.gl/maps/6H2ts6D8QQnboeYcA" target="_blank">
+            Linkstraße 2, 8 OG, 10785, Berlin, Deutschland
+          </a>
+          <h6>Working Hours:</h6>
+          <span>24 hours a day</span>
         </article>
       </section>
       <Map />
---
+++ src/data/socialNetworks.js
@@ -0,0 +1,31 @@
+import facebook from '../assets/socialNetworks/f.svg';
+import instagram from '../assets/socialNetworks/inst.svg';
+import linkedin from '../assets/socialNetworks/in.svg';
+import youtube from '../assets/socialNetworks/yt.svg';
+
+export const social_networks = [
+  {
+    id: 1,
+    name: 'Facebook',
+    logo: facebook,
+    src: 'https://www.facebook.com/telranDE/',
+  },
+  {
+    id: 2,
+    name: 'Instagram',
+    logo: instagram,
+    src: 'https://www.instagram.com/telran.de/',
+  },
+  {
+    id: 3,
+    name: 'Linkedin',
+    logo: linkedin,
+    src: 'https://www.linkedin.com/company/tel-ran-de/about/',
+  },
+  {
+    id: 4,
+    name: 'Youtube',
+    logo: youtube,
+    src: 'https://www.youtube.com/@tel-ran.europe',
+  },
+];
